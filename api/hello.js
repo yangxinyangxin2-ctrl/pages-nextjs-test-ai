37a59c1e0fac04c6bac5dc8d8ce53537b6fb30d3
yangxin <yangxin.feiu@bytedance.com>
2026-03-18 14:53:39 +0800

    feat:更新2027878俄文413133243342wewer

--- api/hello.js
+++ api/hello.js
@@ -1,6 +1,6 @@
 export async function GET(request) {
   return Response.json({
-    message: "Hello from IGA Pages!！！！！！！！！！！！",
+    message: "Hello from IGA Pages!！！！！！！fddfs！！！！！",
     timestamp: new Date().toISOString(),
   });
 }
